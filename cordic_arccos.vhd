d64578077ebbfd19d46b6ff3680a032c77da8a96
NabilRismawan <13223035@std.stei.itb.ac.id>
2024-12-12 11:21:53 +0700

    bismillah bisa rek

--- cordic_arccos.vhd
+++ cordic_arccos.vhd
@@ -1,68 +1,204 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-use IEEE.MATH_REAL.ALL;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.std_logic_arith.ALL;
+USE IEEE.std_logic_unsigned.ALL;
 
 entity cordic_arccos is
-    generic (
-        ITERATIONS : integer := 16; -- Number of iterations
-        WIDTH      : integer := 16  -- Fixed-point width
-    );
     port (
-        clk      : in  std_logic;
-        reset    : in  std_logic;
-        cos_in   : in  signed(WIDTH-1 downto 0); -- Input cosine value
-        angle_out: out signed(WIDTH-1 downto 0)  -- Output angle
+        enable : IN STD_LOGIC;
+		reset : IN STD_LOGIC;
+		clock : IN STD_LOGIC;
+		Xinput : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		done : OUT STD_LOGIC;
+		Zoutput : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
     );
 end entity cordic_arccos;
 
 architecture Behavioral of cordic_arccos is
+    COMPONENT LUT_arctan IS 
+        PORT (
+        iterasi : IN STD_LOGIC_VECTOR (3 downto 0);
+        hasil : OUT STD_LOGIC_VECTOR (15 downto 0)
+        );
+    END component;
 
-    -- Declare real_vector type
-    type real_vector is array (natural range <>) of real;
-
-    -- Precomputed arctangent values in degrees
-    constant ANGLE_TABLE : real_vector(0 to ITERATIONS-1) := (
-        45.0, 26.5650512, 14.0362435, 7.12501635,
-        3.57633437, 1.78991061, 0.89517371, 0.44761417,
-        0.2238105,  0.1119057,  0.0559528,  0.0279764,
-        0.0139882,  0.0069941,  0.003497,   0.0017485
+BEGIN
+    LUT_arctan_inst: entity work.LUT_arctan
+     port map(
+        iterasi => iterasi,
+        hasil => hasil
     );
+	process( rst, clk ) 
+	variable x: std_logic_vector (15 downto 0);   
+	variable y: std_logic_vector (15 downto 0);
+	variable y_o: std_logic_vector (15 downto 0) := "1011011001000110"; -- nilai awal sumbu y
+	variable x_o: std_logic_vector (15 downto 0) := "0000000000000000"; -- nilai awal sumbu x
+	variable theta: std_logic_vector (15 downto 0);
+	variable theta_o: std_logic_vector (15 downto 0) := "0000000000000000"; -- nilai awal theta (yang akan menjadi keluaran sudut)
+	variable shx: std_logic_vector (15 downto 0); 
+	variable shy: std_logic_vector (15 downto 0);
+	
+	begin
+        if( rst = '1' ) then
+            y_o := "1011011001000110";
+            x_o := "0000000000000000";
+            theta_o := "0000000000000000";
+        elsif( clk'event and clk = '1' ) then
+            if iterasi < 10 then
+                    
+                if iterasi = 0 then
+                    if (x_o < x_in) then
+                        y := y_o - x_o;
+                        x := x_o + y_o;
+                        theta := theta_o + nilai_out;
+                    else
+                        y := y_o + x_o;
+                        x := x_o - y_o;
+                        theta := theta_o - nilai_out;
+                    end if;
+                    
+                elsif iterasi = 1 then
+                    shx (15 downto 0) := "0" & x (15 downto 1);
+                    shy (15 downto 0) := "0" & y (15 downto 1);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+                    
+                elsif iterasi = 2 then
+                    shx (15 downto 0) := "00" & x (15 downto 2);
+                    shy (15 downto 0) := "00" & y (15 downto 2);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
 
-    -- Function to convert real to fixed-point signed
-    function real_to_fixed(value : real; bits : integer) return signed is
-        variable fixed_val : integer;
-    begin
-        fixed_val := integer(value * (2.0 ** (bits-1)));
-        return to_signed(fixed_val, bits);
-    end function;
+                elsif iterasi = 3 then
+                    shx (15 downto 0) := "000" & x (15 downto 3);
+                    shy (15 downto 0) := "000" & y (15 downto 3);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
 
-    signal x, y, z : signed(WIDTH-1 downto 0); -- Vector components and angle
-    signal iteration : integer := 0;
+                elsif iterasi = 4 then
+                    shx (15 downto 0) := "0000" & x (15 downto 4);
+                    shy (15 downto 0) := "0000" & y (15 downto 4);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
 
-begin
-    process(clk, reset)
-    begin
-        if reset = '1' then
-            x <= cos_in;           -- Initialize x to input cosine
-            y <= to_signed(0, WIDTH); -- Initialize y to zero
-            z <= to_signed(0, WIDTH); -- Initialize accumulated angle to zero
-            iteration <= 0;
-        elsif rising_edge(clk) then
-            if iteration < ITERATIONS then
-                if x(WIDTH-1) = '1' then -- If x < 0, rotate up
-                    x <= x + (y srl iteration); -- x += y >> iteration
-                    y <= y - (x srl iteration); -- y -= x >> iteration
-                    z <= z - real_to_fixed(ANGLE_TABLE(iteration), WIDTH);
-                else -- Rotate down
-                    x <= x - (y srl iteration); -- x -= y >> iteration
-                    y <= y + (x srl iteration); -- y += x >> iteration
-                    z <= z + real_to_fixed(ANGLE_TABLE(iteration), WIDTH);
+                elsif iterasi = 5 then
+                    shx (15 downto 0) := "00000" & x (15 downto 5);
+                    shy (15 downto 0) := "00000" & y (15 downto 5);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+
+                elsif iterasi = 6 then
+                    shx (15 downto 0) := "000000" & x (15 downto 6);
+                    shy (15 downto 0) := "000000" & y (15 downto 6);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+
+                elsif iterasi = 7 then
+                    shx (15 downto 0) := "0000000" & x (15 downto 7);
+                    shy (15 downto 0) := "0000000" & y (15 downto 7);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+
+                elsif iterasi = 8 then
+                    shx (15 downto 0) := "00000000" & x (15 downto 8);
+                    shy (15 downto 0) := "00000000" & y (15 downto 8);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+
+                elsif iterasi = 9 then
+                    shx (15 downto 0) := "000000000" & x (15 downto 9);
+                    shy (15 downto 0) := "000000000" & y (15 downto 9);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+
+                elsif iterasi = 10 then
+                    shx (15 downto 0) := "0000000000" & x (15 downto 10);
+                    shy (15 downto 0) := "0000000000" & y (15 downto 10);
+                    if (x < x_in) then
+                        y := y - shx;
+                        x := x + shy;
+                        theta := theta + nilai_out;
+                    else
+                        y := y + shx;
+                        x := x - shy;
+                        theta := theta - nilai_out;
+                    end if;
+                
                 end if;
-                iteration <= iteration + 1;
-            else
-                angle_out <= z; -- Output final angle
+            
+                iterasi <= iterasi + 1;
+
             end if;
-        end if;
-    end process;
+
+            sudut <= "1011010000000000" - theta;
+
+            else
+            end if;
+
+        end process;
+
+    end sim;
 end Behavioral;
